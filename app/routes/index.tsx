1fefcdef6f5fd88cac5e53fd5b5cebe0910bc979
Daniel Manfield <daniel.manfield9@gmail.com>
2022-06-28 20:56:42 -0400

    pre commit test four

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,8 @@
 export default function Index() {
   return (
                 <h1 className="text-3xl text-orange-500 font-bold underline">
-          Hello world!
+            Hello world!
     </h1>
+    
   );
 }
