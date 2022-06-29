4feb1dd1c57d347d889dd8aa59d4b6583c22cee9
Daniel Manfield <daniel.manfield9@gmail.com>
2022-06-28 21:18:20 -0400

    precommit test 6

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: [
+      "airbnb-typescript",
+      "airbnb/hooks",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:jest/recommended",
+      "prettier",
+      "plugin:prettier/recommended",
+      "plugin:import/recommended"
+    ],
+    plugins: ["react", "prettier", "@typescript-eslint", "jest"],
+    env: {
+      browser: true,
+      es6: true,
+      jest: true
+    },
+    parserOptions: {
+      project: './tsconfig.json'
+    },
+    rules: {
+      'prettier/prettier': 'error'
+    }
+  } 
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,7 @@
 export default function Index() {
   return (
-                <h1 className="text-3xl text-orange-500 font-bold underline">
-            Hello world!
+    <h1 className="text-3xl text-orange-500 font-bold underline">
+      Hello world!
     </h1>
-    
-
-    
   );
 }
